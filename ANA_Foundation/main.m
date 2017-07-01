e69a539ad9de9259611063f94d6d6390b0bc0918
byxx <byxx@users.noreply.github.com>
2017-07-01 17:37:47 +0200

    Initial Commit

---
+++ ANA_Foundation/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ANA_Foundation
+//
+//  Created by byxx on 01.07.17.
+//  Copyright © 2017 byxx. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
